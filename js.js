f7895637cbc9b8341b353c6b2a1b0ef7e6a1f1b7
Angel0327 <66433690+Angel0327@users.noreply.github.com>
2020-08-12 14:34:27 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+﻿var grupoTarjeta1 = ["🐶", "🐺", "🐱","🐵","💥", "🦧", "🐷","🐪","🦒","🦏","🦛","🐹","🦔","🐨","🐼","🦡","🐣"];
+var grupoTarjeta2 = ["🐶", "🐺", "🐱","🐵","💥", "🦧", "🐷","🐪","🦒","🦏","🦛","🐹","🦔","🐨","🐼","🦡","🐣"];
+
+var todasLasTarjetas = grupoTarjeta1.concat(grupoTarjeta2);
+
+function crearTajetas() {
+  var mesa = document.querySelector("#mesa");
+
+  todasLasTarjetas.forEach(function mensaje(Element) {
+    var tarjeta = document.createElement("div");
+
+    tarjeta.innerHTML = "<p class='ico'>" + Element + "</p>";
+
+    mesa.appendChild(tarjeta);
+  });
+}
+
+crearTajetas();
+
+/*<div class="tarjeta">🐶</div>*/
